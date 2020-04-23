bccd4b8d422421d7af23c671198b8b863646412f
ofeks <ofekn271@gmail.com>
2020-04-23 23:51:23 +0300

    redirect to home if user signed in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -13,8 +14,8 @@ class App extends Component {
   unsubscribedFromauth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
-   
+    const { setCurrentUser } = this.props;
+
     this.unsubscribedFromauth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -42,13 +43,26 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (
+            <Redirect to='/' />
+          ) : (
+              <SignInAndSignUpPage />
+            )
+          }
+          />
         </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+   mapDispatchToProps
+   )
+   (App);
