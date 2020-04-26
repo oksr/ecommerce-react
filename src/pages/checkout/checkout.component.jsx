8f679d94938542e196a19d4e4cc916817b46193a
ofeks <ofekn271@gmail.com>
2020-04-26 12:59:16 +0300

    Checkout item componenet and use cart selector

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from "reselect";
 import { selectCartItems,selectCartTotal } from '../../redux/cart/cart.selectors'
+import CheckoutItem from '../../components/checkout-item.styles.scss/checkout-item.component';
+
 import './checkout.styles.scss';
+
 
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
@@ -25,11 +28,12 @@ const CheckoutPage = ({cartItems, total}) => (
         </div>
         {
             cartItems.map(cartItem => 
-                cartItem.name
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}></CheckoutItem>
                 )
         }
         <div className='total'>
             <span>Total: ${total}</span>
+            
         </div>
     </div>
 )
